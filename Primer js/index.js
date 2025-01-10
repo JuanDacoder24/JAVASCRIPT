5f5f8d6341307b85f1425bcb14163d965a250db0
COMPU-S035\daw1 <Juan David>
2025-01-10 13:36:03 +0100

    primer avance js

---
+++ Primer js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const cuerpo = document.getElementById('cuerpo');
+    const botonRestaurar = document.getElementById('botonRestaurar');
+    const parrafoInteractivo = document.getElementById('parrafoInteractivo');
+})
